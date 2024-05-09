ec03dbb4425e4a08cf13982b7d393793dc77c253
akuttiyan <akuttiyan@eastsideprep.org>
2024-05-09 11:47:43 -0700

    added the exit code to the supplies

--- main.py
+++ main.py
@@ -18,17 +18,16 @@ def choose_archetype():
         print("Ultimate Frisbee Kid-- GPA: 3.7")
         print("Class Ditcher-- GPA: 3.1")
         chosen_archetype = input("Which archetype do you want to be?")
+        player = Player(chosen_archetype)
+
+        return player
+
+player = choose_archetype()
+def buy_supplies(player):
+
+   
 
 
-    player = Player(chosen_archetype)
-
-    print(player.archetype)
-
-def buy_supplies():
-
-
-
-    
     '''
     print("Now it's time to buy supplies. Here are your options:")
     print("1. Books")
@@ -148,7 +147,58 @@ def buy_supplies():
     if keyboard.read_key == "space":
         print("You are ready for the school year")
 '''
+    #space_not_pressed = True
+    supplies_input = "x"
+    while supplies_input != "8":
+        
+        print("Now it's time to buy supplies. Here are your options:")
+        print("1. Books")
+        print("2. Calculator")
+        print("3. Pens/Pencils")
+        print("4. Notebooks")
+        print("5. Computer Quality")
+        print("6. Charger")
+        print("7. Stylus")
+        print("8. Exit")
+        print("Press spacebar to exit the supplies")
+        supplies_input = input("What would you like to buy? (Press the number) ")
 
+        if supplies_input == "1":
+            num_books = input("How many books would you like to buy for the school year? ")
+            player.add_num_books(int(num_books))
+        elif supplies_input == "2":
+            calculator = input("What type of calculator do you want? ")
+            player.add_calc_type(calculator)
+        elif supplies_input == "3":
+            num_pencils = input("How many pens/pencils do you want? ")
+            player.add_num_pens(int(num_pencils))
+        elif supplies_input == "4":
+            num_notebooks = input("How many notebooks do you want? ")
+            player.add_num_notebooks(int(num_notebooks))
+        elif supplies_input == "5":
+            computer_type = input("What computer type do you want? ")
+            player.add_comp_type(computer_type)
+        
+        elif supplies_input == "6":
+            num_chargers = input("How many chargers do you want? ")
+            player.add_num_chargers(int(num_chargers))
+          
+        elif supplies_input == "7":
+            num_styluses = input("How many styluses do you want? ")
+            player.add_num_styluses(int(num_styluses))
+        elif supplies_input == "8":
+            print("Exit")
+
+        else:
+            supplies_input = input("Choose one of the options ")
+
+        print("You are ready for the school year")
+        print("Here are your supplies")
+        print("Notebooks:", player.num_books)
+    
+            
+
+    '''
     while keyboard.read_key != "space":
         
         print("Now it's time to buy supplies. Here are your options:")
@@ -164,30 +214,39 @@ def buy_supplies():
 
         if supplies_input == "1":
             num_books = input("How many books would you like to buy for the school year? ")
-            player.add_num_books()
+            player.add_num_books(int(num_books))
         elif supplies_input == "2":
             calculator = input("What type of calculator do you want? ")
+            player.add_calc_type(calculator)
         elif supplies_input == "3":
             num_pencils = input("How many pens/pencils do you want? ")
+            player.add_num_pens(int(num_pencils))
         elif supplies_input == "4":
             num_notebooks = input("How many notebooks do you want? ")
+            player.add_num_notebooks(int(num_notebooks))
         elif supplies_input == "5":
             computer_type = input("What computer type do you want? ")
+            player.add_comp_type(computer_type)
         
         elif supplies_input == "6":
             num_chargers = input("How many chargers do you want? ")
+            player.add_num_chargers(int(num_chargers))
           
         elif supplies_input == "7":
             num_styluses = input("How many styluses do you want? ")
+            player.add_num_styluses(int(num_styluses))
         else:
             supplies_input = input("Choose one of the options ")
-            
     
             
 
     if keyboard.read_key == "space":
         print("You are ready for the school year")
-            
+
+    '''
+
+#chosen_archetype = choose_archetype()      
+buy_supplies(player)     
             
     #pass
 
@@ -201,5 +260,3 @@ def game_loop():
 
 def finish():
     pass
-
-buy_supplies()
